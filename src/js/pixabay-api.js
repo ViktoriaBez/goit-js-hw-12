69ba73c49331c5c248b9cf19d5859601269c4fc3
ViktoriaBez <154977573+ViktoriaBez@users.noreply.github.com>
2024-06-08 09:56:36 +0300

    task

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,18 +1,19 @@
-const userGallery = userSearch => {
-    const searchParams = new URLSearchParams({
-        key: '43654443-0aa2ac279d0400a8e119738dd',
-        q: userSearch,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: 'true'
-    });
-    const url = `https://pixabay.com/api/?${searchParams}`;
-    return fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(response.status);
-            }
-            return response.json();
-        })
+import axios from 'axios';
+export const perPage = 15;
+
+const userGallery = async (userSearch, myPage) => {
+    const response = await axios.get('https://pixabay.com/api/', {
+        params: {
+            key: '43654443-0aa2ac279d0400a8e119738dd',
+            q: userSearch,
+            image_type: 'photo',
+            orientation: 'horizontal',
+            safesearch: 'true',
+            per_page: perPage,
+            page: myPage
+        }
+    })
+    return response.data;
 };
+
 export default userGallery;
--- src/main.js
+++ src/main.js
@@ -5,22 +5,71 @@ import addImages from "./js/render-functions";
 const form = document.querySelector('form');
 const gallery = document.querySelector('.gallery');
 const loader = document.querySelector('.loader');
+const btn = document.querySelector('.load');
+btn.style.display = 'none';
+import { perPage } from "./js/pixabay-api";
+let myPage = 1;
+let userSearch;
 
-form.addEventListener('submit', (event) => {
+form.addEventListener('submit', async event => {
     event.preventDefault();
-    const userSearch = event.target.elements.search.value.trim();
+    userSearch = event.target.elements.search.value.trim();
     loader.style.display = 'flex';
+    btn.style.display = 'none';
     gallery.innerHTML = '';
-    userGallery(userSearch)
-        .then(response => {
-            if (response.hits.length === 0) {
-                iziToast.show({
-                    message: 'Sorry, there are no images matching your search query. Please try again!'
-                })
-            }
 
+    try {
+        myPage = 1;
+        const response = await userGallery(userSearch, myPage);
+        if (response.hits.length === 0) {
+            btn.style.display = 'none';
+            iziToast.show({
+                message: 'Sorry, there are no images matching your search query. Please try again!'
+            })
+        } else {
             addImages(response)
-        })
-        .catch(error => console.error('Error', error))
-        .finally(() => loader.style.display = 'none');
+            btn.style.display = 'flex';
+
+            if (response.totalHits <= 15) {
+                btn.style.display = 'none';
+            }
+        }
+    } catch (error) {
+        console.error(error);
+    } finally {
+        loader.style.display = 'none';
+    }
+});
+
+const scroll = () => {
+    const galleryItem = document.querySelector('.gallery-item');
+    const itemHeight = galleryItem.getBoundingClientRect().height;
+    window.scrollBy({
+        top: itemHeight * 2,
+        behavior: 'smooth'
+    });
+};
+
+btn.addEventListener('click', async () => {
+    myPage += 1;
+
+    try {
+        const response = await userGallery(userSearch, myPage);
+        const totalPages = Math.ceil(response.totalHits / perPage);
+
+        loader.style.display = 'flex';
+        addImages(response)
+        scroll();
+        if (myPage >= totalPages) {
+            btn.style.display = 'none';
+            return iziToast.error({
+                position: "topRight",
+                message: "We're sorry, there are no more posts to load"
+            });
+        }
+    } catch (error) {
+        console.error(error);
+    } finally {
+        loader.style.display = 'none';
+    }
 });
